df371d7dd3f51bf69d7b162068de26a8a98b559e
Viraj Gavade <vrajgavade17@gmail.com>
2025-04-20 16:21:46 +0530

    Listing model created

---
+++ Models/listings.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+
+const ListingSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    catgory: {
+        type: String,
+        enum: ['electronics', 'furniture', 'clothing', 'books', 'others'],
+        required: true,
+    },
+    images: [String],
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    isSolde: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+
+const ListingModel = mongoose.model('Listing', ListingSchema);
+module.exports = ListingModel;
