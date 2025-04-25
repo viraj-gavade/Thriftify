0020276a6e37c53d4b756134c0614c7fb0772b90
Viraj Gavade <vrajgavade17@gmail.com>
2025-04-25 15:56:40 +0530

    updated the chat schema

---
+++ Schemas/chat.schemas.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const ChatSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  message: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Chat', ChatSchema);
