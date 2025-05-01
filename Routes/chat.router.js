f38abbf852064a85b0b5053ec91c079d15e55118
Viraj Gavade <vrajgavade17@gmail.com>
2025-05-01 16:14:34 +0530

    `Update authentication middleware in chat router to use VerifyJwt instead of isAuthenticated`

--- Routes/chat.router.js
+++ Routes/chat.router.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const chatController = require('../Controllers/chat.controllers');
-const { isAuthenticated } = require('../Middleware/auth.middleware');
+const VerifyJwt = require('../Middlewares/authentication.middleware');
+
 
 // Get all chat rooms for the user
-router.get('/rooms', isAuthenticated, chatController.getUserChatRooms);
+router.get('/rooms',VerifyJwt ,chatController.getUserChatRooms);
 
 // Get a specific chat room by ID
-router.get('/rooms/:roomId', isAuthenticated, chatController.getChatRoomById);
+router.get('/rooms/:roomId',VerifyJwt ,chatController.getChatRoomById);
 
 // Create a new chat room
-router.post('/rooms', isAuthenticated, chatController.createChatRoom);
+router.post('/rooms', VerifyJwt,chatController.createChatRoom);
 
 // Send a new message
-router.post('/rooms/:roomId/messages', isAuthenticated, chatController.sendMessage);
+router.post('/rooms/:roomId/messages',VerifyJwt ,chatController.sendMessage);
 
 // Legacy endpoint
 router.get('/:user1/:user2', chatController.getChatBetweenUsers);
