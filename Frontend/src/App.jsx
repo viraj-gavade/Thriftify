3930a61ec8eff3f73ebd6d27dbec320bf166f623
Viraj Gavade <vrajgavade17@gmail.com>
2025-05-19 17:15:08 +0530

    Intial Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+
+  return (
+    <>
+
+    <h1 className="bg-red-700">React Migration</h1>
+    <h1 className="bg-blue-700">React Migration</h1>
+    </>
+  )
+}
+
+export default App
